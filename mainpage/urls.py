9c8dc94142375ce0991db9f5289acf307e2e1b24
kazickiirs <kazickiirs@cps32.ru>
2023-08-24 18:14:23 +0300

    Добавил проекты

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -1,5 +1,7 @@
+from django.conf.urls.static import static
 from django.urls import path
 from mainpage import views
+from metall import settings
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -7,4 +9,4 @@ urlpatterns = [
     path('navesy/', views.shelter, name='shelter'),
     path('zabori/', views.fence, name='fence'),
     path('besedki/', views.pergola, name='pergola'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mainpage/views.py
+++ mainpage/views.py
@@ -1,9 +1,25 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from mainpage.models import Stair,Shelter, Pergola, Fence
+import random
+
+
+def get_random_project(query_set):
+    if len(query_set) > 0:
+        return list(query_set)[random.randint(0, len(query_set)-1)]
 
 
 def home(request):
-    return render(request, 'mainpage/home.html')
+    random_stair = get_random_project(Stair.objects.all())
+    random_fence = get_random_project(Fence.objects.all())
+    random_shelter = get_random_project(Shelter.objects.all())
+    random_pergola = get_random_project(Pergola.objects.all())
+    random_projects = [random_stair, random_fence, random_shelter, random_pergola]
+
+    context = {
+        'Random_projects': random_projects
+    }
+    return render(request, 'mainpage/home.html', context)
 
 
 def stair(request):
