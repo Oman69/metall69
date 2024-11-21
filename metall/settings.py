8c813797737a349e3897d69950523b1b9059933a
kazickiirs <kazickiirs>
2024-11-21 18:05:03 +0300

    Начал переработку дизайна

--- metall/settings.py
+++ metall/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'mainpage',
     'embed_video',
-    'phonenumber_field'
+    'phonenumber_field',
+    'livereload'
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'metall.urls'
